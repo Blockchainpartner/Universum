75873847b2251d89119c5db4a5b04b74d9090f35
Florian Bouchut <fl.bouchut@gmail.com>
2023-03-15 17:40:43 +0100

    Add TailwindCSS to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
