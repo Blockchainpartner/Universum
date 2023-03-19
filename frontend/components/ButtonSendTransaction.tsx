1360849a9015df0b7902e54252342969207c9ff8
Florian Bouchut <fl.bouchut@gmail.com>
2023-03-20 00:02:30 +0100

    Update UI

--- frontend/components/ButtonSendTransaction.tsx
+++ frontend/components/ButtonSendTransaction.tsx
@@ -1,22 +1,73 @@
 import React, { useState } from 'react'
 import { sendMetaTx2 } from "../service/metaTx"
+import { useContractRead } from 'wagmi'
+import useDebounce from './useDebounce'
+const namehash = require('eth-ens-namehash');
+let ensABI = require('../service/ensABI.js');
+
 
 
 const ButtonSendTransaction = () => {
     const [name, setName] = useState('');
+    const debouncedName = useDebounce(name, 500);
+    const [isFree, setIsFree] = useState(false);
+    const [isTaken, setIsTaken] = useState(false);
+
+    const { refetch } = useContractRead(
+        {
+            address: '0x9EBEe49a631f179f927B721d526080c100A82C4D',
+            abi: ensABI,
+            functionName: 'owner',
+            args: [namehash.hash(name + ".test")],
+        }
+    );
 
     async function handleClick(name: string) {
         sendMetaTx2(name);
-    }
+    };
 
+    async function handleVerify() {
+        setIsFree(false);
+        setIsTaken(false);
+        const res = await refetch();
+        const stringRes = res.data?.toString();
+        console.log("resultat de l'appel: ", stringRes);
+        if (stringRes == "0x0000000000000000000000000000000000000000") {
+            setIsFree(true);
+        }
+        else {
+            setIsTaken(true);
+        }
+    };
 
     return (
-        <div>
-            <input
-                onChange={(e) => setName(e.target.value)}
-                placeholder="ens name"
-                value={name}></input>
-            <button onClick={() => handleClick(name)}>Lancer la metaTx</button>
+        <div className='flex flex-col'>
+            <div className='flex flex-row'>
+                <input
+                    className='border rounded p-2'
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Search name"
+                    value={name}>
+                </input>
+                <p className='flex justify-center items-center'>.uni</p>
+                <button
+                    className='border rounded p-2 ml-4'
+                    onClick={() => handleVerify()}
+                >Search</button>
+            </div>
+            {isFree && (
+                <>
+                    <p className='text-green-500 p-2'>The name is available</p>
+                    <button
+                        className='border rounded p-2'
+                        onClick={() => handleClick(name)}>Register it on all L2's
+                    </button>
+                    <h1 className='p-4 font-bold text-lg text-center'>Supported networks</h1>
+                </>
+            )}
+            {isTaken && (
+                <p className='text-red-500 p-2'>The name is already registered</p>
+            )}
         </div>
     )
 }
--- frontend/service/registrar.js
+++ frontend/service/registrar.js
@@ -1,65 +1,5 @@
 import { ethers } from 'ethers';
-
-const abi = [
-    {
-      "inputs": [
-        {
-          "internalType": "contract ENS",
-          "name": "ensAddr",
-          "type": "address"
-        },
-        {
-          "internalType": "bytes32",
-          "name": "node",
-          "type": "bytes32"
-        },
-        {
-          "internalType": "contract MinimalForwarder",
-          "name": "trustedForwarder",
-          "type": "address"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "forwarder",
-          "type": "address"
-        }
-      ],
-      "name": "isTrustedForwarder",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "bytes32",
-          "name": "label",
-          "type": "bytes32"
-        },
-        {
-          "internalType": "address",
-          "name": "owner",
-          "type": "address"
-        }
-      ],
-      "name": "register",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ];
+let registrarAbi = require('./registrarABI.js');
 
 //const address = process.env.NEXT_PUBLIC_FORWARDER_CONTRACT_ADDRESS
 const address = "0x070E42eB3C33c4b001ef3Dd3098818dFE1aaDf18"
@@ -67,5 +7,5 @@ const address = "0x070E42eB3C33c4b001ef3Dd3098818dFE1aaDf18"
 //ENS REGISTRY 0x9EBEe49a631f179f927B721d526080c100A82C4D
 
 export function createInstanceRegistrar(provider) {
-  return new ethers.Contract(address, abi, provider);
+  return new ethers.Contract(address, registrarAbi, provider);
 }
