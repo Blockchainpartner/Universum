3194f2ee15d9c6b9bc058730c5b874468decfe8f
hazem <hazemdhaou.it@gmail.com>
2023-03-25 11:43:17 +0100

    :lipstick: fixed astro position

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -5,8 +5,8 @@ import headerImg from "../assets/astro.svg";
 const Footer = () => {
   return (
     <>
-      <div className="box-content -translate-y-36 w-1/2  translate-x-full">
-        <div className={" flex justify-end animate-waving-hand "}>
+      <div className="box-content -translate-y-44 w-1/2  translate-x-full">
+        <div className={"flex justify-end animate-waving-hand "}>
           <Image
             className={""}
             src={headerImg}
