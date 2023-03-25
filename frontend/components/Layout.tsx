9cca55637718ce6f37b1b2bb5ce8d2d379fe4540
hazem <hazemdhaou.it@gmail.com>
2023-03-25 01:13:07 +0100

    :wrench: added notistack config

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,14 +2,21 @@ import React, { PropsWithChildren } from "react";
 import "animate.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { SnackbarProvider } from "notistack";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-screen bg-hero">
-      <Navbar />
-      {children}
-      <Footer />
-    </div>
+    <SnackbarProvider
+      maxSnack={1}
+      autoHideDuration={2500}
+      anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+    >
+      <div className="h-screen bg-hero">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </SnackbarProvider>
   );
 };
 export default Layout;
